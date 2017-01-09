02d51ac20801991cb0a4f1109a5c773e32098502
Eric Löffler <eric.loeffler@opalia.systems>
2017-01-09 22:04:39 +0100

    add improvements for a Scala friendly usage of JSR 310

--- src/main/scala/systems/opalia/commons/misc/Improvements.scala
+++ src/main/scala/systems/opalia/commons/misc/Improvements.scala
@@ -1,5 +1,9 @@
 package systems.opalia.commons.misc
 
+import java.time.temporal.{ChronoUnit, TemporalAmount}
+import java.time.{Duration => JDuration, _}
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.language.implicitConversions
 
 
@@ -64,6 +68,30 @@ object Improvements {
 
     def big: BigDecimal =
       BigDecimal.decimal(x)
+
+    def dayAsPeriod: Period =
+      daysAsPeriod
+
+    def daysAsPeriod: Period =
+      Period.ofDays(x)
+
+    def weekAsPeriod: Period =
+      weeksAsPeriod
+
+    def weeksAsPeriod: Period =
+      Period.ofWeeks(x)
+
+    def monthAsPeriod: Period =
+      monthsAsPeriod
+
+    def monthsAsPeriod: Period =
+      Period.ofMonths(x)
+
+    def yearAsPeriod: Period =
+      yearsAsPeriod
+
+    def yearsAsPeriod: Period =
+      Period.ofYears(x)
   }
 
   implicit class LongImprovements(x: Long) {
@@ -82,6 +110,123 @@ object Improvements {
 
     def big: BigDecimal =
       BigDecimal.decimal(x)
+  }
+
+  implicit class ScalaDurationImprovements(x: Duration) {
+
+    def chronoUnit: ChronoUnit =
+      x.unit match {
+        case TimeUnit.DAYS => ChronoUnit.DAYS
+        case TimeUnit.HOURS => ChronoUnit.HOURS
+        case TimeUnit.MINUTES => ChronoUnit.MINUTES
+        case TimeUnit.SECONDS => ChronoUnit.SECONDS
+        case TimeUnit.MILLISECONDS => ChronoUnit.MILLIS
+        case TimeUnit.MICROSECONDS => ChronoUnit.MICROS
+        case TimeUnit.NANOSECONDS => ChronoUnit.NANOS
+      }
+
+    def asJava: JDuration =
+      JDuration.of(x.length, x.chronoUnit)
+  }
+
+  implicit class JavaDurationImprovements(x: JDuration)
+    extends Ordered[JDuration] {
+
+    def +(that: JDuration): JDuration =
+      x.plus(that)
+
+    def -(that: JDuration): JDuration =
+      x.minus(that)
+
+    def +(that: FiniteDuration): JDuration =
+      x.plus(that.length, that.chronoUnit)
+
+    def -(that: FiniteDuration): JDuration =
+      x.minus(that.length, that.chronoUnit)
+
+    def *(scalar: Long): JDuration =
+      x.multipliedBy(scalar)
+
+    def /(divisor: Long): JDuration =
+      x.dividedBy(divisor)
+
+    def unary_! : JDuration =
+      x.negated
+
+    def compare(that: JDuration): Int =
+      x.compareTo(that)
+  }
+
+  implicit class JavaPeriodImprovements(x: Period) {
+
+    def +(that: Period): Period =
+      x.plus(that)
+
+    def -(that: Period): Period =
+      x.minus(that)
+
+    def *(scalar: Int): Period =
+      x.multipliedBy(scalar)
+
+    def unary_! : Period =
+      x.negated
+  }
+
+  implicit class JavaInstantImprovements(x: Instant)
+    extends Ordered[Instant] {
+
+    def +(that: TemporalAmount): Instant =
+      x.plus(that)
+
+    def -(that: TemporalAmount): Instant =
+      x.minus(that)
+
+    def +(that: FiniteDuration): Instant =
+      x.plus(that.length, that.chronoUnit)
+
+    def -(that: FiniteDuration): Instant =
+      x.minus(that.length, that.chronoUnit)
+
+    def compare(that: Instant): Int =
+      x.compareTo(that)
+  }
+
+  implicit class JavaOffsetDateTimeImprovements(x: OffsetDateTime)
+    extends Ordered[OffsetDateTime] {
+
+    def +(that: TemporalAmount): OffsetDateTime =
+      x.plus(that)
+
+    def -(that: TemporalAmount): OffsetDateTime =
+      x.minus(that)
+
+    def +(that: FiniteDuration): OffsetDateTime =
+      x.plus(that.length, that.chronoUnit)
+
+    def -(that: FiniteDuration): OffsetDateTime =
+      x.minus(that.length, that.chronoUnit)
+
+    def compare(that: OffsetDateTime): Int =
+      x.compareTo(that)
+  }
+
+  implicit class JavaOffsetTimeImprovements(x: OffsetTime)
+    extends Ordered[OffsetTime] {
+
+    def +(that: TemporalAmount): OffsetTime =
+      x.plus(that)
+
+    def -(that: TemporalAmount): OffsetTime =
+      x.minus(that)
+
+    def +(that: FiniteDuration): OffsetTime =
+      x.plus(that.length, that.chronoUnit)
+
+    def -(that: FiniteDuration): OffsetTime =
+      x.minus(that.length, that.chronoUnit)
+
+    def compare(that: OffsetTime): Int =
+      x.compareTo(that)
   }
 
 }
---
+++ src/test/scala/systems/opalia/commons/misc/ImprovementsTest.scala
@@ -0,0 +1,108 @@
+package systems.opalia.commons.misc
+
+import java.time.temporal.ChronoUnit
+import java.time.{Duration => JDuration, _}
+import org.scalatest._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import systems.opalia.commons.misc.Improvements._
+
+
+class ImprovementsTest
+  extends FlatSpec
+    with Matchers {
+
+  it should "be easier to use (java.time.Duration)" in {
+
+    val duration1 = JDuration.ofNanos(999)
+    val duration2 = duration1.plus(3, ChronoUnit.DAYS)
+
+    val scalaDuration = 3 days
+    val javaDuration = (3 days).asJava
+
+    (duration1 + scalaDuration) should be(duration2)
+    (duration1 + javaDuration) should be(duration2)
+
+    (duration1 * 2) should be((1998 nanoseconds).asJava)
+    (duration1 / 2) should be((499 nanoseconds).asJava)
+    (!duration1) should be((-999 nanoseconds).asJava)
+
+    duration1 == duration2 should be(false)
+    duration1 != duration2 should be(true)
+    duration1 < duration2 should be(true)
+    duration1 <= duration2 should be(true)
+    duration1 > duration2 should be(false)
+    duration1 >= duration2 should be(false)
+  }
+
+  it should "be easier to use (java.time.Period)" in {
+
+    val period = 28 weeksAsPeriod
+
+    (period + period) should be(56 weeksAsPeriod)
+
+    (period * 2) should be(56 weeksAsPeriod)
+    (!period) should be(-28 weeksAsPeriod)
+  }
+
+  it should "be easier to use (java.time.Instant)" in {
+
+    val instant1 = Instant.EPOCH
+    val instant2 = instant1.plus(73, ChronoUnit.DAYS)
+
+    val scalaDuration = 73 days
+    val javaDuration = (73 days).asJava
+    val javaPeriod = 73 daysAsPeriod
+
+    (instant1 + scalaDuration) should be(instant2)
+    (instant1 + javaDuration) should be(instant2)
+    (instant1 + javaPeriod) should be(instant2)
+
+    instant1 == instant2 should be(false)
+    instant1 != instant2 should be(true)
+    instant1 < instant2 should be(true)
+    instant1 <= instant2 should be(true)
+    instant1 > instant2 should be(false)
+    instant1 >= instant2 should be(false)
+  }
+
+  it should "be easier to use (java.time.OffsetDateTime)" in {
+
+    val dateTime1 = Instant.EPOCH.atOffset(ZoneOffset.UTC)
+    val dateTime2 = dateTime1.plus(42, ChronoUnit.DAYS)
+
+    val scalaDuration = 42 days
+    val javaDuration = (42 days).asJava
+    val javaPeriod = 42 daysAsPeriod
+
+    (dateTime1 + scalaDuration) should be(dateTime2)
+    (dateTime1 + javaDuration) should be(dateTime2)
+    (dateTime1 + javaPeriod) should be(dateTime2)
+
+    dateTime1 == dateTime2 should be(false)
+    dateTime1 != dateTime2 should be(true)
+    dateTime1 < dateTime2 should be(true)
+    dateTime1 <= dateTime2 should be(true)
+    dateTime1 > dateTime2 should be(false)
+    dateTime1 >= dateTime2 should be(false)
+  }
+
+  it should "be easier to use (java.time.OffsetTime)" in {
+
+    val time1 = OffsetTime.of(0, 0, 0, 0, ZoneOffset.UTC)
+    val time2 = time1.plus(42, ChronoUnit.HOURS)
+
+    val scalaDuration = 42 hours
+    val javaDuration = (42 hours).asJava
+
+    (time1 + scalaDuration) should be(time2)
+    (time1 + javaDuration) should be(time2)
+
+    time1 == time2 should be(false)
+    time1 != time2 should be(true)
+    time1 < time2 should be(true)
+    time1 <= time2 should be(true)
+    time1 > time2 should be(false)
+    time1 >= time2 should be(false)
+  }
+}
