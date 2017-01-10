3c2830f174ec1a1284d906e18739f3479b1eb690
Eric Löffler <eric.loeffler@opalia.systems>
2017-01-10 01:05:47 +0100

    add improvements for java.nio.file.Paths

--- src/main/scala/systems/opalia/commons/misc/Improvements.scala
+++ src/main/scala/systems/opalia/commons/misc/Improvements.scala
@@ -1,8 +1,10 @@
 package systems.opalia.commons.misc
 
+import java.nio.file.{Path, Paths}
 import java.time.temporal.{ChronoUnit, TemporalAmount}
 import java.time.{Duration => JDuration, _}
 import java.util.concurrent.TimeUnit
+import scala.collection.JavaConverters._
 import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.language.implicitConversions
 
@@ -110,6 +112,65 @@ object Improvements {
 
     def big: BigDecimal =
       BigDecimal.decimal(x)
+  }
+
+  implicit class JavaPathImprovements(x: Path)
+    extends Iterable[Path]
+      with Ordered[Path] {
+
+    def absolute: Path =
+      if (!x.isAbsolute)
+        Paths.get("/").resolve(x)
+      else
+        x
+
+    def relative: Path =
+      if (x.isAbsolute)
+        Paths.get("/").relativize(x)
+      else
+        x
+
+    def common(that: Path): Path = {
+
+      def build(a: List[Path], b: List[Path]): Path =
+        (a, b) match {
+          case (a +: as, b +: bs) if (a == b) => a.resolve(build(as, bs))
+          case _ => Paths.get("")
+        }
+
+      if (x.isAbsolute == that.isAbsolute) {
+
+        if (x.isAbsolute)
+          build(this.toList, that.toList).absolute
+        else
+          build(this.toList, that.toList).relative
+
+      } else
+        Paths.get("")
+    }
+
+    def uncommon(that: Path): Path = {
+
+      val commonSize = this.common(that).size
+
+      if (commonSize == 0)
+        x
+      else if (commonSize == x.size)
+        Paths.get("")
+      else
+        x.subpath(commonSize, x.size)
+    }
+
+    def iterator: Iterator[Path] = {
+
+      if (x == Paths.get(""))
+        Nil.iterator
+      else
+        x.iterator().asScala
+    }
+
+    def compare(that: Path): Int =
+      x.compareTo(that)
   }
 
   implicit class ScalaDurationImprovements(x: Duration) {
--- src/test/scala/systems/opalia/commons/misc/ImprovementsTest.scala
+++ src/test/scala/systems/opalia/commons/misc/ImprovementsTest.scala
@@ -1,5 +1,6 @@
 package systems.opalia.commons.misc
 
+import java.nio.file.Paths
 import java.time.temporal.ChronoUnit
 import java.time.{Duration => JDuration, _}
 import org.scalatest._
@@ -11,6 +12,51 @@ import systems.opalia.commons.misc.Improvements._
 class ImprovementsTest
   extends FlatSpec
     with Matchers {
+
+  it should "be able to find common path (java.nio.file.Paths)" in {
+
+    val p1 = Paths.get("/home/joe")
+    val p2 = Paths.get("/home/joe/foo")
+    val p3 = Paths.get("/home/joe/bar")
+
+    val p4 = Paths.get("")
+
+    p1.common(p1) should be(p1)
+
+    p1.common(p3) should be(p1)
+
+    p2.common(p3) should be(p1)
+
+    p1.relative.common(p3.relative) should be(p1.relative)
+
+    p2.relative.common(p3.relative) should be(p1.relative)
+
+    p2.common(p3.relative) should be(p4)
+  }
+
+  it should "be able to find uncommon path (java.nio.file.Paths)" in {
+
+    val p1 = Paths.get("/home/joe/foo/bar")
+    val p2 = Paths.get("/home/joe")
+    val p3 = Paths.get("foo/bar")
+
+    val p4 = Paths.get("/home/joe/foo")
+    val p5 = Paths.get("/home/joe/bar")
+    val p6 = Paths.get("foo")
+    val p7 = Paths.get("bar")
+
+    val p8 = Paths.get("")
+
+    p1.uncommon(p1) should be(p8)
+
+    p1.uncommon(p1.relative) should be(p1)
+
+    p1.uncommon(p2) should be(p3)
+
+    p4.uncommon(p5) should be(p6)
+
+    p5.uncommon(p4) should be(p7)
+  }
 
   it should "be easier to use (java.time.Duration)" in {
 
