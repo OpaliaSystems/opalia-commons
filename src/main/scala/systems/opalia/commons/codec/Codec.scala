0c4a7eb804b52b84accc38de9fd7cf4305955e27
Eric Löffler <eric.loeffler@opalia.systems>
2018-11-04 16:38:37 +0100

    add more functions to codec classes

--- src/main/scala/systems/opalia/commons/codec/Codec.scala
+++ src/main/scala/systems/opalia/commons/codec/Codec.scala
@@ -1,11 +1,31 @@
 package systems.opalia.commons.codec
 
 
-trait Codec[T] {
+trait Codec {
 
-  def encode(data: T): String
+  def encode(data: Seq[Byte]): String
 
-  def decode(data: String): Option[T]
+  def encode(data: Seq[Byte], charset: String): String
+
+  def encodeFromString(data: String): String
+
+  def encodeFromString(data: String, charset: String): String
+
+  def decode(data: String): IndexedSeq[Byte]
+
+  def decode(data: String, charset: String): IndexedSeq[Byte]
+
+  def decodeToString(data: String): String
+
+  def decodeToString(data: String, charset: String): String
+
+  def decodeOpt(data: String): Option[IndexedSeq[Byte]]
+
+  def decodeOpt(data: String, charset: String): Option[IndexedSeq[Byte]]
+
+  def decodeToStringOpt(data: String): Option[String]
+
+  def decodeToStringOpt(data: String, charset: String): Option[String]
 
   def isValid(data: String): Boolean
 }
